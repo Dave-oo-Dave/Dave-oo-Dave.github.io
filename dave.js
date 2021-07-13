07b3945aa3d5f163a483703d1dc8c6fe4bedb0ae
Dewanand Giri <nationconvo@gmail.com>
2021-07-13 11:32:31 +0545

    Adding css new

--- dave.js
+++ dave.js
@@ -1,17 +1,28 @@
-const mainMenu = document.querySelector('.mainMenu');
-const closeMenu = document.querySelector('.closeMenu');
-const openMenu = document.querySelector('.openMenu');
+var slideIndex = 1;
+showSlides(slideIndex);
 
-
-
-
-openMenu.addEventListener('click',show);
-closeMenu.addEventListener('click',close);
-
-function show(){
-    mainMenu.style.display = 'flex';
-    mainMenu.style.top = '0';
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
-function close(){
-    mainMenu.style.top = '-100%';
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+} 
